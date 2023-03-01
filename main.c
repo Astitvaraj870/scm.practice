af134c9b7a72ecae2d86b7ac54f10022fa909d5d
Astitvaraj870 <124043701+Astitvaraj870@users.noreply.github.com>
2023-03-01 15:24:18 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+//     int days,year,month,week;
+    // printf("Enter your days: ");
+    // scanf("%d",&days);
+    // year=days/365;
+    // days=days-(365*year);
+    // month=days/30;
+    // days=days-(month*30);
+    // week=days/7;
+    // days=days-(7*week);
+    // printf("year: %d",year);
+    // printf("\n");
+    // printf("month: %d",month);
+    // printf("\n");
+    // printf("week: %d",week);
+    // printf("\n");
+    // printf("days: %d",days);
+    // printf("\n");
+
+// SWITCH
+int a,b,c;
+a=6;
+b=4;
+c=2;
+// scanf("%d",&a);
+// (a%2==0)? printf("This is even."): printf("number is odd.");
+switch(a/b){
+case 1:
+
+
+}
+    return 0;
+}
